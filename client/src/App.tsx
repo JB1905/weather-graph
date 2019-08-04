1103ef04d7b139d6cf8866971b320c6e94b63991
Jakub Biesiada <biesiadajakub@icloud.com>
2019-08-04 14:17:06 +0200

    Update

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import City from './pages/City';
+
+import Layout from './components/Layout';
+import Global from './components/Global';
+import Background from './components/Background';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Router basename="/">
+        <Global />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/city/:id" component={City} />
+          <Redirect from="*" to="/" />
+        </Switch>
+
+        <Background />
+      </Router>
+    </Layout>
+  );
+};
+
+export default App;
