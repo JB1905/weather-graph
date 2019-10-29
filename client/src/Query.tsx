11dd509dce6855dd0ef610d4c4e8e2855ef6c43d
Jakub Biesiada <biesiadajakub@icloud.com>
2019-10-29 21:36:20 +0100

    Update Apollo Client to v3 beta

--- client/src/Query.tsx
+++ client/src/Query.tsx
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import {gql} from '@apollo/client';
 
 export const FORECAST_QUERY = gql`
   query Location($name: String!) {
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-boost';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
 
 import App from './App';
 
