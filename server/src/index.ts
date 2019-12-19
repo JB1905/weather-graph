ad2f0edd16edb9f531f61c3b84b56466bad7c882
Jakub Biesiada <biesiadajakub@icloud.com>
2019-12-19 13:31:49 +0100

    Update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
-import { ApolloServer } from 'apollo-server';
-import { config } from 'dotenv';
+import { ApolloServer } from "apollo-server";
+import { config } from "dotenv";
 
 config();
 
 const server = new ApolloServer({
   modules: [
-    require('./modules/current-weather'),
-    require('./modules/forecast'),
-    require('./modules/uv-index'),
-    // require('./modules/map'),
-    require('./modules/cities')
+    require("./modules/current-weather"),
+    require("./modules/forecast"),
+    require("./modules/uv-index"),
+    require("./modules/cities")
   ]
   // playground: true,
   // introspection: true
