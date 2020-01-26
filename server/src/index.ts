6fa095dd61ca669d76a860ee19f316e2753bf2c3
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-26 10:07:33 +0100

    Remove playground

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,15 @@
-import { ApolloServer } from "apollo-server";
-import { config } from "dotenv";
+import { ApolloServer } from 'apollo-server';
+import { config } from 'dotenv';
 
 config();
 
 const server = new ApolloServer({
   modules: [
-    require("./modules/current-weather"),
-    require("./modules/forecast"),
-    require("./modules/uv-index"),
-    require("./modules/cities")
+    require('./modules/current-weather'),
+    require('./modules/forecast'),
+    require('./modules/uv-index'),
+    require('./modules/cities')
   ]
-  // playground: true,
-  // introspection: true
 });
 
 server.listen().then(({ url }: { url: string }) => {
